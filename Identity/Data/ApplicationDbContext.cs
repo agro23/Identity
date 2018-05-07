57c1b5bee7d5ddbe87ca18d56a31e01d0c962c54
Andy Grossberg <andy.grossberg@gmail.com>
2018-05-07 16:44:24 -0700

    save to change database

--- Identity/Data/ApplicationDbContext.cs
+++ Identity/Data/ApplicationDbContext.cs
@@ -10,10 +10,19 @@ namespace Identity.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //{
+        //    options.UseMySql(@"Server=localhost;Port=8889;database=identity;uid=root;pwd=root;");
+        //}
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -22,5 +31,40 @@ namespace Identity.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        //public DbSet<Identity.Models.BlogPost> BlogPost { get; set; }
+        //public DbSet<Identity.Models.ApplicationUser> ApplicationUser { get; set; }
+        //public DbSet<Identity.Models.Comment> Comment { get; set; }
     }
 }
+
+//public class IdentityDbContext : IdentityDbContext<ApplicationUser>
+//{
+//    public DbSet<BlogPost> BlogPosts { get; set; }
+//    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+//    public DbSet<Comment> Comments { get; set; }
+
+//    public IdentityDbContext(DbContextOptions options) : base(options)
+//    {
+//    }
+
+//    public IdentityDbContext()
+//    {
+//    }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder options)
+//    {
+//        options.UseMySql(@"Server=localhost;Port=8889;database=identity;uid=root;pwd=root;");
+//    }
+
+//    protected override void OnModelCreating(ModelBuilder builder)
+//    {
+//        base.OnModelCreating(builder);
+//    }
+
+//    public DbSet<Identity.Models.BlogPost> BlogPost { get; set; }
+//    public DbSet<Identity.Models.ApplicationUser> ApplicationUser { get; set; }
+//    public DbSet<Identity.Models.Comment> Comment { get; set; }
+
+//}
+//}
